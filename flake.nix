cde9ea2eb67b26a4c5b41b84449e6520a3d744cc
Jonathan Stewart <jonathanstewart@Jonathans-MacBook-Pro-3.local>
2026-06-16 14:27:49 -0700

    Add Nix development flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Development shell for stewartbiology.com Hugo site";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            git
+            go
+            hugo
+          ];
+
+          shellHook = ''
+            echo "Hugo development shell ready: $(hugo version)"
+          '';
+        };
+      });
+}
